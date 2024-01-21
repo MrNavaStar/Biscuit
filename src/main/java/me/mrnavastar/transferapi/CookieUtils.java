f27add9c113703684fea7f448f89d9b7fbb98a95
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-21 16:52:45 -0700

    make CookieUtils use System.arraycopy()

--- src/main/java/me/mrnavastar/transferapi/CookieUtils.java
+++ src/main/java/me/mrnavastar/transferapi/CookieUtils.java
@@ -2,7 +2,6 @@ package me.mrnavastar.transferapi;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -29,15 +28,16 @@ public class CookieUtils {
     // Returns the signed cookie if all is well
     public static byte[] signCookie(byte[] cookie, byte[] secret, Mac mac) throws InvalidKeyException {
         if (cookie == null || secret == null) return null;
-        int len = cookie.length + mac.getMacLength();
+
+        int macLen = mac.getMacLength();
+        int len = cookie.length + macLen;
         if (len > 5120) return null; // Cookie is longer than the max size the client accepts
         if (secret.length == 0) return cookie;
 
-        ByteBuffer buffer = ByteBuffer.wrap(new byte[len]);
-        buffer.put(computeSignature(cookie, secret, mac));
-        buffer.put(cookie);
-
-        return buffer.array();
+        byte[] data = new byte[len];
+        System.arraycopy(computeSignature(cookie, secret, mac), 0, data, 0, macLen);
+        System.arraycopy(cookie, 0, data, macLen, cookie.length);
+        return data;
     }
 
     // Returns original cookie if secret has a length of zero
@@ -46,13 +46,11 @@ public class CookieUtils {
         if (cookie == null || secret == null) return null;
         if (secret.length == 0) return cookie;
 
-        ByteBuffer buffer = ByteBuffer.wrap(cookie);
         int macLen = mac.getMacLength();
-
         byte[] signature = new byte[macLen];
         byte[] data = new byte[cookie.length - macLen];
-        buffer.get(0, signature, 0, macLen);
-        buffer.get(macLen, data, 0, data.length);
+        System.arraycopy(cookie, 0, signature, 0, macLen);
+        System.arraycopy(cookie, macLen, data, 0, data.length);
 
         if (Arrays.equals(signature, computeSignature(data, secret, mac))) return data;
         return null;
